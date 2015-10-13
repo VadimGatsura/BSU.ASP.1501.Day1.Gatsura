7af04374391a3d9e3e8a18269460e3b2ba9ddd3b
Vadim Gatsura <vadimgatsura@gmail.com>
2015-10-13 23:44:04 +0300

    Add the first task.

---
+++ Task1.Test/NewtonMethodTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task1.Test {
+    [TestClass]
+    public class NewtonMethodTest {
+        [TestMethod]
+        public void TestRootValue() {
+            Assert.AreEqual( NewtonMethod.Root(4,2,0.000001), Math.Round(Math.Pow(4, 1.0 / 2), 6) );
+            Assert.AreEqual( NewtonMethod.Root(32, 5, 0.000001), Math.Round(Math.Pow(32, 1.0 / 5), 6) );
+
+            Assert.AreEqual( NewtonMethod.Root(2, 3, 0.000001), Math.Round(Math.Pow(2, 1.0 / 3), 6) );
+            Assert.AreEqual( NewtonMethod.Root(0, 2, 0.000001), Math.Round(Math.Pow(0, 1.0 / 2), 6) );
+
+            Assert.AreEqual( NewtonMethod.Root(-8, 3, 0.000001), Math.Round(Math.Pow(8, 1.0 / 3), 6) * (-1) );  //multiple by (-1), because MathPow(-8, 1.0 / 3) return NaN. That's not true.
+
+        }
+
+        [TestMethod]
+        public void TestPrecision() {
+            Assert.AreEqual( NewtonMethod.Root(10, 2, 0.000001), 3.162277, 0.000001 );
+            Assert.AreEqual( NewtonMethod.Root(10, 2, 1), 3, 1 );
+        }
+
+        [TestMethod]
+        public void TestNaN() {
+            Assert.AreEqual( NewtonMethod.Root(-2, 2, 0.000001), Math.Round(Math.Pow(-2, 1d / 2), 6) );
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestArgumentExceptions() {
+            NewtonMethod.Root(2, 2, -1);
+            NewtonMethod.Root(2, 2, 1.3);
+            NewtonMethod.Root(2, 2, 0);
+        }
+    }
+}
---
+++ Task1/NewtonMethod.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom;
+
+namespace Task1 {
+    public static class NewtonMethod {
+        #region Static Methods
+        /// <summary>Finding of root</summary>
+        /// <param name="value">Value for which you want to search the root</param>
+        /// <param name="n">Degree of root</param>
+        /// <param name="precision">Precision of the found root<remarks>Must be greater than 0 and less than or equal 1</remarks></param>
+        /// <returns>Root of <paramref name="n"/>-degree for the <paramref name="value"/></returns>
+        public static double Root(double value, uint n, double precision) {
+            if (precision < 0 || precision > 1) {
+                throw new ArgumentException(nameof(precision));
+            }
+
+            if (value < 0 && n % 2 == 0)
+                return double.NaN;
+            if (Math.Abs(value) < Math.Abs(precision))
+                return 0;
+            double k = value;
+            double x0 = value;
+            double x1 = x0 - (Math.Pow(x0, n) - k)/(Math.Pow(x0, n - 1)*n);
+
+            while (Math.Abs(x1 - x0) > precision) {
+                x0 = x1;
+                x1 = x0 - (Math.Pow(x0, n) - k)/(Math.Pow(x0, n - 1)*n);
+            }
+
+            for (int i = 0;; i++) {
+                if (precision * Math.Pow(10, i) > 1)
+                    return Math.Round(x1, i);
+            }
+        }
+        #endregion
+    }
+}
