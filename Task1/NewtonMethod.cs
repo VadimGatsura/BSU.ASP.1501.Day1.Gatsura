1ed38153ca382821881fd0a9533774d267cc8edf
Vadim Gatsura <vadimgatsura@gmail.com>
2015-10-14 23:06:28 +0300

    Add tests for Task2

--- Task1/NewtonMethod.cs
+++ Task1/NewtonMethod.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.CodeDom;
 
 namespace Task1 {
     public abstract class NewtonMethod {
---
+++ Task2.Test/JaggedArraySortTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task2.Test {
+    [TestClass]
+    public class JaggedArraySortTest {
+        [TestMethod]
+        public void TestAscRowSum() {
+            int[][] array = { new []{4, 2, -1, 9, -8, 10}, new[]{0}, null, null, new []{4, 3, 8, 5,7} };
+            int[][] testArray = { new[] { 0 }, new[] { 4, 2, -1, 9, -8, 10 }, new[] { 4, 3, 8, 5, 7 }, null, null };
+            JaggedArraySort.SortByRowSum(array, true);
+
+            IStructuralEquatable arrays = array;
+
+            Assert.AreEqual(arrays.Equals(testArray, StructuralComparisons.StructuralEqualityComparer), true);
+        }
+
+        [TestMethod]
+        public void TestDescRowSum() {
+            int[][] array = { new[] { 4, 2, -1, 9, -8, 10 }, new[] { 0 }, null, null, new[] { 4, 3, 8, 5, 7 } };
+            int[][] testArray = { new[] { 4, 3, 8, 5, 7 }, new[] { 4, 2, -1, 9, -8, 10 }, new[] { 0 }, null, null };
+            JaggedArraySort.SortByRowSum(array, false);
+
+            IStructuralEquatable arrays = array;
+
+            Assert.AreEqual(arrays.Equals(testArray, StructuralComparisons.StructuralEqualityComparer), true);
+        }
+
+        [TestMethod]
+        public void TestAscMaxElement() {
+            int[][] array = { new[] { 4, 2, -1, 9, -8, 10 }, new[] { 0 }, null, null, new[] { 4, 3, 8, 5, 7 } };
+            int[][] testArray = { new[] { 0 }, new[] { 4, 3, 8, 5, 7 }, new[] { 4, 2, -1, 9, -8, 10 }, null, null };
+            JaggedArraySort.SortByMaxInRow(array, true);
+
+            IStructuralEquatable arrays = array;
+
+            Assert.AreEqual(arrays.Equals(testArray, StructuralComparisons.StructuralEqualityComparer), true);
+        }
+
+        [TestMethod]
+        public void TestDescMaxElement() {
+            int[][] array = { new[] { 4, 2, -1, 9, -8, 10 }, new[] { 0 }, null, null, new[] { 4, 3, 8, 5, 7 } };
+            int[][] testArray = { new[] { 4, 2, -1, 9, -8, 10 }, new[] { 4, 3, 8, 5, 7 },  new[] { 0 }, null, null };
+            JaggedArraySort.SortByMaxInRow(array, false);
+
+            IStructuralEquatable arrays = array;
+
+            Assert.AreEqual(arrays.Equals(testArray, StructuralComparisons.StructuralEqualityComparer), true);
+        }
+
+        [TestMethod]
+        public void TestAscMinElement() {
+            int[][] array = { new[] { 4, 2, -1, 9, -8, 10 }, new[] { 0 }, null, null, new[] { 4, 3, 8, 5, 7 } };
+            int[][] testArray = { new[] { 4, 2, -1, 9, -8, 10 }, new[] { 0 }, new[] { 4, 3, 8, 5, 7 }, null, null };
+            JaggedArraySort.SortByMinInRow(array, true);
+
+            IStructuralEquatable arrays = array;
+
+            Assert.AreEqual(arrays.Equals(testArray, StructuralComparisons.StructuralEqualityComparer), true);
+        }
+
+        [TestMethod]
+        public void TestDescMinElement() {
+            int[][] array = { new[] { 4, 2, -1, 9, -8, 10 }, new[] { 0 }, null, null, new[] { 4, 3, 8, 5, 7 } };
+            int[][] testArray = { new[] { 4, 3, 8, 5, 7 },  new[] { 0 }, new[] { 4, 2, -1, 9, -8, 10 }, null, null };
+            JaggedArraySort.SortByMinInRow(array, false);
+
+            IStructuralEquatable arrays = array;
+
+            Assert.AreEqual(arrays.Equals(testArray, StructuralComparisons.StructuralEqualityComparer), true);
+        }
+    }
+}
