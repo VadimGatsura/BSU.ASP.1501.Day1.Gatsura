211b631b215b06c22d0d2453f7ad8f44852cd585
Vadim Gatsura <vadimgatsura@gmail.com>
2015-10-17 16:10:46 +0300

    Refactor of Task1

--- Task1.Test/NewtonMethodTest.cs
+++ Task1.Test/NewtonMethodTest.cs
@@ -6,32 +6,32 @@ namespace Task1.Test {
     public class NewtonMethodTest {
         [TestMethod]
         public void TestIntRootValue() {
-            Assert.AreEqual( NewtonMethod.Root(32, 5, 6), Math.Pow(32, 1.0 / 5), 0.000001  );
+            Assert.AreEqual( NewtonMethod.Root(32, 5, 0.000001), Math.Pow(32, 1.0 / 5), 0.000001  );
         }
 
         [TestMethod]
         public void TestDoubleRootValue() {
-            Assert.AreEqual(NewtonMethod.Root(32, 7, 6), Math.Pow(32, 1.0/7), 0.000001);
+            Assert.AreEqual(NewtonMethod.Root(32, 7, 0.000001), Math.Pow(32, 1.0/7), 0.000001);
         }
 
         [TestMethod]
         public void TestNaNValue() {
-            Assert.AreEqual(NewtonMethod.Root(double.NaN, 4, 5), double.NaN);
+            Assert.AreEqual(NewtonMethod.Root(double.NaN, 4, 0.1), double.NaN);
         }
 
         [TestMethod]
         public void TestPrecision() {
-            Assert.AreEqual( NewtonMethod.Root(10, 2, 1), 3, 1 );
+            Assert.AreEqual( NewtonMethod.Root(10, 2, 0.1), Math.Pow(10, 1.0 / 2), 0.1 );
         }
 
         [TestMethod]
         public void TestMaxPrecision() {
-            Assert.AreEqual(NewtonMethod.Root(10, 2, 15), Math.Pow(10, 1.0 / 2), 0.000000000000001);
+            Assert.AreEqual(NewtonMethod.Root(10, 2, 0.000000000000001), Math.Pow(10, 1.0 / 2), 0.000000000000001);
         }
 
         [TestMethod]
         public void TestNaNRoot() {
-            Assert.AreEqual( NewtonMethod.Root(-2, 2, 6), Math.Round(Math.Pow(-2, 1d / 2), 6) );
+            Assert.AreEqual( NewtonMethod.Root(-2, 2, 0.1), Math.Pow(-2, 1d / 2) );
         }
 
         [TestMethod]
@@ -43,17 +43,17 @@ namespace Task1.Test {
 
         [TestMethod]
         public void TestDoubleValue() {
-            Assert.AreEqual( NewtonMethod.Root(5.6, 2, 6), Math.Pow(5.6, 1.0 / 2), 0.000001);
+            Assert.AreEqual( NewtonMethod.Root(5.6, 2, 0.000001), Math.Pow(5.6, 1.0 / 2), 0.000001);
         }
 
         [TestMethod]
         public void TestNegativeValue() {
-            Assert.AreEqual(NewtonMethod.Root(-8, 3, 6), Math.Pow(8, 1.0 / 3) * (-1), 0.000001);  //multiple by (-1), because MathPow(-8, 1.0 / 3) return NaN. That's not true.    
+            Assert.AreEqual(NewtonMethod.Root(-8, 3, 0.000001), Math.Pow(8, 1.0 / 3) * (-1), 0.000001);  //multiple by (-1), because MathPow(-8, 1.0 / 3) return NaN. That's not true.    
         }
 
         [TestMethod]
         public void TestZeroValue() {
-            Assert.AreEqual(NewtonMethod.Root(0, 2, 6), Math.Pow(0, 1.0 / 2), 0.000001);
+            Assert.AreEqual(NewtonMethod.Root(0, 2, 0.000001), Math.Pow(0, 1.0 / 2), 0.000001);
         }
 
     }
--- Task1/NewtonMethod.cs
+++ Task1/NewtonMethod.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 
+
 namespace Task1 {
-    public abstract class NewtonMethod {
+    public static class NewtonMethod {
         #region Static Methods
         /// <summary>Finding of root</summary>
         /// <param name="value">Value for which you want to search the root</param>
         /// <param name="n">Degree of root</param>
-        /// <param name="digits">The number of fractional digits in root<remarks>Must be between 0 and 15, included 15</remarks></param>
+        /// <param name="precision">Precision of root</param>
         /// <returns>Root of <paramref name="n"/>-degree for the <paramref name="value"/></returns>
-        public static double Root(double value, uint n, uint digits) {
-            if(digits > 15)
-                throw new ArgumentOutOfRangeException(nameof(digits));
+        public static double Root(double value, uint n, double precision) {
+            if(precision < 1E-16 || precision > 1)
+                throw new ArgumentOutOfRangeException(nameof(precision));
 
             if (value < 0 && n % 2 == 0)
                 return double.NaN;
 
-            if (Math.Abs(value) < Math.Pow(0.1, digits))
+            if (Math.Abs(value) < precision)
                 return 0;
 
             double x0, x1 = value, k = value; 
@@ -23,9 +24,10 @@ namespace Task1 {
             do {
                 x0 = x1;
                 x1 = x0 - (Math.Pow(x0, n) - k)/(Math.Pow(x0, n - 1)*n);
-            } while (Math.Abs(x1 - x0) > Math.Pow(0.1, digits));
+            } while (Math.Abs(x1 - x0) > precision);
 
-            return Math.Round(x1, (int) digits);
+
+            return x1;
         }
         #endregion
     }
